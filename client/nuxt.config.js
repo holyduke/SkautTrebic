3a4d07f023255fda22432bb34d2dabce10c68ed1
Dominik Jasek <jasekdominik@seznam.cz>
2021-01-03 21:58:09 +0100

    rename folder

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -61,6 +61,12 @@ export default {
     }
   },
 
+  // watchers: {
+  //   webpack: {
+  //     poll: true
+  //   }
+  // },
+
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     { src: '~/plugins/gallery', mode: 'client' }
